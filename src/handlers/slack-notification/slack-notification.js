6e573357a0cd64293494b0cd477ff28566a27001
nancy <ndo@seek.com.au>
2021-08-16 19:52:35 +1000

    add link

--- src/handlers/slack-notification/slack-notification.js
+++ src/handlers/slack-notification/slack-notification.js
@@ -9,12 +9,9 @@ module.exports.handler = (snsEvent) => {
 
   // Send the notification
   // Print out a link to the logs 
-  // https://console.aws.amazon.com/cloudwatch/home?region=ap-southeast-2#logEventViewer:group=/aws/lambda/simple-project-dev-slack-notifications
   (async () => {
     await webhook.send({
-      title: 'Error',
-      text: 'Whoopsie!! Looks like your lambda had an error. Have a cookie!',
-      icon_emoji: ':speak-no-evil:'
+      text: 'Whoopsie!! Looks like your lambda had an error. Check out the log <a href="https://console.aws.amazon.com/cloudwatch/home?region=ap-southeast-2#logEventViewer:group=/aws/lambda/simple-project-dev-slack-notifications">here.</a>',
     });
   })();
 
